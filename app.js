20e4595e83c842a333011b757a5ef2120d3d294e
Anuj Pardeshi <anujpardeshi500@gmail.com>
2025-01-14 13:14:11 +0530

    Add new files

---
+++ app.js
@@ -0,0 +1,302 @@
+let myList = [
+    {
+        img: "https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/5795/1735727595795-i"
+    },
+    {
+        img: "https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/3092/1735609143092-i"
+    },
+    {
+        img: "https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/2162/1735709092162-i"
+    },
+
+]
+
+let myList1 = [
+        {
+        name: "The Legend of Hanuman",
+        img: "https://image.tmdb.org/t/p/w500/AdCWLnuw01cWkdy7nsZVtnX4KVn.jpg",
+        },
+        {
+            name:"Bagheera",
+            img: "https://m.media-amazon.com/images/M/MV5BYTFiZWM0ZDYtZjg0ZS00ZTc1LWFiMzAtOWI5MWYwYTFiZjkxXkEyXkFqcGc@._V1_.jpg"
+        },
+        {
+            name:"Skeleton Crew",
+            img: "https://lumiere-a.akamaihd.net/v1/images/skeleton-crew-teaster-poster-4866453125478789_3179e794.jpeg?region=0,0,729,1080"
+        },
+        {
+            name:"Harikatha",
+            img: "https://m.media-amazon.com/images/M/MV5BMzNmZTAxZmQtMDIxYi00NDllLWE1NjItN2YxMjY4ODUzMjllXkEyXkFqcGc@._V1_.jpg"
+        },
+        {
+            name:"Nemesis",
+            img: "https://assets.gadgets360cdn.com/pricee/assets/product/202410/Nemesis_poster_1_1728985272.jpg"
+        },
+        {
+        name:"Light Shop",
+        img: "https://m.media-amazon.com/images/M/MV5BZTY2MjNjNTUtNzM5ZS00OTVmLWIxMGEtOTUwZGM0OGI1MjkxXkEyXkFqcGc@._V1_.jpg"
+        },
+        {
+        name: "Brave New Art",
+        img: "https://m.media-amazon.com/images/M/MV5BZDJjMDc1NzEtNTVkMS00YTczLTk1YzQtN2JhYWVhYWM4YzY5XkEyXkFqcGc@._V1_QL75_UX190_CR0,28,190,281_.jpg"
+        },
+        {
+        name:"Sugarcane",
+        img: "https://m.media-amazon.com/images/M/MV5BNDk0MzM0MmQtM2U2Mi00MmY0LWIxM2EtZDJjZWZiZDA1YTk2XkEyXkFqcGc@._V1_.jpg"
+        },
+        {
+        name:"Heartbeat",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRN974RVgPteVDljvX-guJleSqviBu4k9fhfdltMNAdgDZcYPLOFdlyzqI5rYvRyIqgE-o&usqp=CAU"
+        },
+        {
+        name:"TSUNAMI",
+        img: "https://images.justwatch.com/poster/322845336/s718/season-1.jpg"
+        },
+        {
+        name:"ITA Awards",
+        img: "https://pbs.twimg.com/media/GgHE4WUbkAAeKrs.jpg"
+        },
+        {
+        name: "What IF",
+        img: "https://www.syfy.com/sites/syfy/files/styles/scale_600/public/what-if-_s1_ka_zombieironman_v1_lg.jpg"
+        },
+        {
+        name: "Gunaah",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo4pMxt5_AoeoxNFJSLbKY7tlnY6CJDb-avQ&s"
+        },
+        {
+        name: "Big Boss",
+        img: "https://indiantvinfo.com/media/2024/10/Tamil-Bigg-Boss-8-Live-Streaming-409x550.jpg"
+        },
+        {
+        name: "Nuvvunte naa jathaga",
+        img: "https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/nuvvunte-naa-jathaga-et00320693-28-12-2021-04-38-25.jpg"
+        },
+        {
+        name: "Inside out",
+        img: "https://image.tmdb.org/t/p/original/8XOctvsqbVsis3XJrmV8Pqh0BYx.jpg"
+        },
+        {
+        name: "All we image as light",
+        img: "https://m.media-amazon.com/images/M/MV5BNzg5OWVkODQtNDViMS00YmFkLTg4YjctMGFmZjM4OTdlYWRhXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+        }
+]
+
+let myList2 = [
+    {
+        name: "Brentford Vs Arsenal",
+        img: "https://img10.hotstar.com/image/upload/f_auto,q_90,w_384/sources/r1/cms/prod/1879/1735649221879-h" 
+    },
+    {
+        name: "Melbourne Renegades Vs Adelaide Strikers",
+        img: "https://img10.hotstar.com/image/upload/f_auto,q_90,w_384/sources/r1/cms/prod/6942/1735727626942-h" 
+    },
+    {
+        name: "5th Test: All you need",
+        img: "https://img10.hotstar.com/image/upload/f_auto,q_90,w_384/sources/r1/cms/prod/7441/1735709117441-h" 
+    },
+    {
+        name: "Goals of MW 19 ft. Jota",
+        img: "https://img10.hotstar.com/image/upload/f_auto,q_90,w_384/sources/r1/cms/prod/4278/1735766314278-h" 
+    },
+    {
+        name: "Goals of MW 19 ft. Pickford",
+        img: "https://img10.hotstar.com/image/upload/f_auto,q_90,w_384/sources/r1/cms/prod/9270/1735766599270-h" 
+    },
+    {
+        name: "Stars gets first win as Heat",
+        img: "https://img10.hotstar.com/image/upload/f_auto,q_90,w_384/sources/r1/cms/prod/4789/1735731914789-h" 
+    },
+    {
+        name: "Sixers Face a Hurricane",
+        img: "https://img10.hotstar.com/image/upload/f_auto,q_90,w_384/sources/r1/cms/prod/7999/1735716737999-h" 
+    },
+    {
+        name: "Hits & Flops: Ipswich Town",
+        img: "https://img10.hotstar.com/image/upload/f_auto,q_90,w_384/sources/r1/cms/prod/3490/1735621263490-h" 
+    }
+]
+
+let top_list = [
+    {
+        name:"Bagheera",
+        img: "top-1.png",
+        img: "https://m.media-amazon.com/images/M/MV5BYTFiZWM0ZDYtZjg0ZS00ZTc1LWFiMzAtOWI5MWYwYTFiZjkxXkEyXkFqcGc@._V1_.jpg",
+    },
+    {
+        name: "Thukra ke mera pyar",
+        img: "https://m.media-amazon.com/images/M/MV5BYzhlOGM4MmMtNDc1NS00Mjk2LTgxMTEtZDZkOGRiZjdhYTYyXkEyXkFqcGc@._V1_.jpg",
+    },
+    {
+        name: "Sarfira",
+        img: "https://images.plex.tv/photo?size=large-1920&scale=1&url=https%3A%2F%2Fmetadata-static.plex.tv%2F0%2Fgracenote%2F0548f9f55edfd8483d42e30fa48613c7.jpg",
+    },
+    {
+        name: "salaar",
+        img: "https://wallpapercave.com/wp/wp13540562.jpg"
+    },
+    {
+        name: "The Legend of Hanuman",
+        img: "https://image.tmdb.org/t/p/w500/AdCWLnuw01cWkdy7nsZVtnX4KVn.jpg"
+    },
+    {
+        name: "Taaza khabar",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoZ07tNKp26gg8NTlr5PkoU2dw8m_OgPHg68jgKUZI9xDqg1st8_DNbD7TEoqAKZrtdWM&usqp=CAU"
+    },
+    {
+        name: "Kill",
+        img: "https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/kill-et00374797-1718179453.jpg",
+    },
+    {
+        name: "Aveshyam",
+        img: "https://image.tmdb.org/t/p/original/k5RWPaNjgRcNvGoawYaQHQwyctI.jpg",
+    },
+    {
+        name: "Deadpool and Wolverine",
+        img: "https://m.media-amazon.com/images/M/MV5BZTk5ODY0MmQtMzA3Ni00NGY1LThiYzItZThiNjFiNDM4MTM3XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    },
+    {
+        name: "Munjhya",
+        img: "https://image.tmdb.org/t/p/original/vLHJMSY7nCSHD46fgQG8MMxbWh2.jpg",
+    }
+]
+
+// Slideshow
+localStorage.setItem("slide_data",JSON.stringify(myList))
+let slide_list = JSON.parse(localStorage.getItem("slide_data"));
+console.log(slide_list)
+
+let currentIndex = 0;
+let myBox = document.getElementById("slider")
+
+function displaySlide(slide_list, currentIndex) {
+    myBox.innerHTML= "";
+    let el = slide_list[currentIndex];
+    let div = document.createElement("div");
+    div.className ="slide"
+    let img = document.createElement("img");
+    img.src = el.img;
+    div.append(img);
+    myBox.append(div);
+}
+
+displaySlide(slide_list, currentIndex);
+
+setInterval(function(){
+    currentIndex = (currentIndex + 1) % slide_list.length;
+    displaySlide(slide_list, currentIndex);
+}, 300000)
+
+// swipe button
+window.onload=function(){
+    let ml=1330;
+    let ml1=1340;
+    document.getElementById("swipe-left").addEventListener("click", function(){
+        
+    })
+    
+    document.getElementById("swipe-right").addEventListener("click", function(){
+        let fullWidth=document.getElementById("movie-slider").offsetWidth;
+        let remainingWidth=fullWidth;
+        if(remainingWidth>ml){
+            document.getElementById("movie-slider").style.marginLeft=-ml+"px";
+            remainingWidth=remainingWidth-1330;
+            ml=ml+1330;
+
+        } else if(remainingWidth <= ml){
+            document.getElementById("movie-slider").style.marginLeft=-(fullWidth) +"px";
+        }
+        console.log("swipe right");
+    });
+    document.getElementById("swipe-left1").addEventListener("click", function(){
+        console.log
+    })
+    
+    document.getElementById("swipe-right1").addEventListener("click", function(){
+        let fullWidth=document.getElementById("sports-slider").offsetWidth;
+        let remainingWidth=fullWidth;
+        if(remainingWidth>ml1){
+            document.getElementById("sports-slider").style.marginLeft=-ml1+"px";
+            remainingWidth=remainingWidth-1340;
+            ml1=ml1+1340;
+
+        } else if(remainingWidth<=ml1){
+            document.getElementById("sports-slider").style.marginLeft=-(fullWidth) +"px";
+        }
+        console.log("swipe right");
+    });
+    document.getElementById("swipe-right2").addEventListener("click", function(){
+        let fullWidth=document.getElementById("toplist-slider").offsetWidth;
+        let remainingWidth=fullWidth;
+        if(remainingWidth>ml1){
+            document.getElementById("toplist-slider").style.marginLeft=-ml1+"px";
+            remainingWidth=remainingWidth-1340;
+            ml1=ml1+1340;
+
+        } else if(remainingWidth<=ml1){
+            document.getElementById("toplist-slider").style.marginLeft=-(fullWidth) +"px";
+        }
+        console.log("swipe right");
+    });
+};
+
+
+
+
+// Movie List
+localStorage.setItem("movie_data",JSON.stringify(myList1))
+let myMovieList = JSON.parse(localStorage.getItem("movie_data"));
+console.log(myMovieList)
+display1(myMovieList)
+function display1(myMovieList) {
+    let myBox1 = document.getElementById("movie")
+    myBox1.innerHTML= null
+    myMovieList.map(function(el,index){
+        let div = document.createElement("div")
+        let name = document.createElement("h1")
+        name.innerText = el.name;
+        let img = document.createElement("img")
+        img.src = el.img;
+        div.append(img, name)
+        myBox1.append(div)
+    })
+}
+
+// Sports Section
+localStorage.setItem("sports_data", JSON.stringify(myList2))
+let sports_arr = JSON.parse(localStorage.getItem("sports_data"));
+console.log(sports_arr)
+display2(sports_arr)
+
+
+function display2(sports_arr) {
+    let myBox2 = document.getElementById("sport")
+    myBox2.innerHTML = null
+    sports_arr.map(function(el, index){
+        let div = document.createElement("div")
+        let name = document.createElement("h1")
+        name.innerText = el.name;
+        let img = document.createElement("img")
+        img.src = el.img;
+        div.append(img, name)
+        myBox2.append(div)
+    })
+}
+// Top 10 Section
+localStorage.setItem("top_data", JSON.stringify(top_list))
+let top_list_arr = JSON.parse(localStorage.getItem("top_data"));
+console.log(top_list_arr)
+display3(top_list_arr)
+
+
+function display3(top_list_arr) {
+    let myBox3 = document.getElementById("top10")
+    myBox3.innerHTML = null
+    top_list_arr.map(function(el, index){
+        let div = document.createElement("div")
+        let img = document.createElement("img")
+        img.src = el.img;
+        div.append(img)
+        myBox3.append(div)
+    })
+}
